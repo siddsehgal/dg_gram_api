518c36512ab76f50d0f322f7f74d426609f3dfd9
sid-metaversity <siddharth@metaversitytechnologies.com>
2023-02-26 12:23:42 +0530

    PORT

--- src/socket-controller/index.js
+++ src/socket-controller/index.js
@@ -3,7 +3,7 @@ import { Server } from 'socket.io';
 import messageController from './messageController.js';
 
 export default function connectSocket(server) {
-  const io = new Server(5501, { cors: { origin: '*' } });
+  const io = new Server(process.env.PORT, { cors: { origin: '*' } });
 
   io.on('connection', (socket) => {
     console.log('Connected: ', socket.id);
