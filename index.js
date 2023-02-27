db54e64ca655e852959e98bd2e9999cb6bb13ec6
sid-metaversity <siddharth@metaversitytechnologies.com>
2023-02-27 18:26:17 +0530

    Added

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-await MySqlDB();
 ConnectSocket();
+await MySqlDB();
 
 app.use(json());
 
--- src/socket-controller/index.js
+++ src/socket-controller/index.js
@@ -3,7 +3,7 @@ import { Server } from 'socket.io';
 import messageController from './messageController.js';
 
 export default function connectSocket(server) {
-  const io = new Server({ cors: { origin: '*' } });
+  const io = new Server(process.env.PORT, { cors: { origin: '*' } });
 
   io.on('connection', (socket) => {
     console.log('Connected: ', socket.id);
